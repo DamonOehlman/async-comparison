6460c09569126df47eb2a05bc6c91239167cf340
jkroso <jkroso@gmail.com>
2013-07-24 00:04:56 +1200

    simplify CPS implementation

--- child-dirs/callbacks-using-async/index.js
+++ child-dirs/callbacks-using-async/index.js
@@ -1,29 +1,17 @@
-var fs = require('fs');
-var path = require('path');
+
 var async = require('async');
+var path = require('path');
+var fs = require('fs');
 
-function findSamples(targetPath, callback) {
-  fs.readdir(targetPath, function(err, files) {
+function findSamples(dir, callback) {
+  fs.readdir(dir, function(err, files) {
     if (err) return callback(err);
-
-    // get the full path names of the files
-    files = files.map(function(name){
-      return path.join(targetPath, name)
-    });
-
-    // stat each of the files
-    async.map(files, fs.stat, function(err, results) {
-      var matchingFiles;
-
-      if (err) return callback(err);
-
-      // remove files that aren't a directory
-      matchingFiles = files.filter(function(filename, index) {
-        return results[index].isDirectory();
-      });
-
-      callback(null, matchingFiles.map(path.basename));
-    });
+    async.filter(files, function(name, done){
+      fs.stat(dir + '/' + name, function(e, stat){
+        if (e) callback(e)
+        else done(stat.isDirectory())
+      })
+    }, callback);
   });
 }
 
