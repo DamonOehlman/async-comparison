da36d36e4ae8982950b17b1e7791609d646d75e8
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-24 01:44:30 +0100

    read child-dirs without any outside aids

---
+++ child-dirs/nude/index.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+
+function readDirs(dir, cb) {
+  fs.readdir(dir, function (err, dir) {
+    if(err) return cb(err)
+    var n = 0, dirs = []
+    dir.forEach(function (d) {
+      n++
+      fs.stat(d, function (err, stat) {
+        if(err) return cb(err)
+        if(stat.isDirectory()) dirs.push(d)
+        next()
+      })
+    })
+    function next () {
+      if(--n) return
+      cb(null, dirs)
+    }
+    if(!n) next()
+  })
+}
+
+if(!module.parent)
+  readDirs(process.cwd(), console.log)
